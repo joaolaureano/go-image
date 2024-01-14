d36521e68264a32108a539fca62680670cbec292
João Pedro Laureano <laureano.pedrojoao@gmail.com>
2024-01-14 20:08:44 -0300

    test

--- main.go
+++ main.go
@@ -8,22 +8,26 @@ import (
 	_ "image/jpeg"
 	_ "image/png"
 	"os"
+	"runtime"
 	"sync"
+	"time"
 )
 
-type PixelCoordinates struct {
-	MaxPixel [2]int
-	MinPixel [2]int
-}
+var numOfThreads = runtime.NumCPU()
 
 func main() {
-	img := openImage("./download.jpeg")
-	p := distributePixels(img.Bounds(), 1)
+	start := time.Now()
+	defer func() {
+		fmt.Printf("took %v\n", time.Since(start))
+	}()
+	img := openImage("./test_images/download.jpeg")
+	imgBounds := img.Bounds()
+	p := distributePixels(imgBounds.Max.Y, numOfThreads)
 	var wg sync.WaitGroup
 	wg.Add(len(p))
-	result := image.NewRGBA(img.Bounds())
-	for _, pixels := range p {
-		go removeComponentFilter(result, pixels, &wg)
+	result := image.NewRGBA(imgBounds)
+	for _, rows := range p {
+		go removeComponentFilter(img, result, rows, &wg)
 	}
 	wg.Wait()
 
@@ -36,7 +40,6 @@ func main() {
 	jpeg.Encode(noRedFile, result, nil)
 
 }
-
 func openImage(filename string) image.Image {
 	file, err := os.Open(filename)
 	if err != nil {
@@ -52,73 +55,28 @@ func openImage(filename string) image.Image {
 	return img
 
 }
-
-//	func applyGrayscaleFilter(img image.Image) image.Image {
-//		bounds := img.Bounds()
-//		result := image.NewRGBA(bounds)
-//
-//		for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
-//			for x := bounds.Min.X; x < bounds.Max.X; x++ {
-//				pixel := img.At(x, y)
-//				originalColor := color.RGBAModel.Convert(pixel).(color.RGBA)
-//				grayValue := (originalColor.R + originalColor.G + originalColor.B) / 3
-//				grayPixel := color.RGBA{R: grayValue, G: grayValue, B: grayValue, A: originalColor.A}
-//				result.Set(x, y, grayPixel)
-//			}
-//		}
-//
-//		return result
-//	}
-func removeComponentFilter(img *image.RGBA, coordinates PixelCoordinates, wg *sync.WaitGroup) {
+func removeComponentFilter(image image.Image, img *image.RGBA, rows []int, wg *sync.WaitGroup) {
 	defer wg.Done()
-	maxPixel := coordinates.MaxPixel
-	minPixel := coordinates.MinPixel
 
-	for y := minPixel[1]; y <= maxPixel[1]; y++ {
-		for x := minPixel[0]; x <= maxPixel[0]; x++ {
-			pixel := (*img).At(x, y)
-			originalColor := color.RGBAModel.Convert(pixel).(color.RGBA)
+	for y := rows[0]; y <= rows[1]; y++ {
+		for x := img.Bounds().Min.X; x <= img.Bounds().Max.X; x++ {
+			originalColor := color.RGBAModel.Convert((image).At(x, y)).(color.RGBA)
 			originalColor.G = 0
 			img.Set(x, y, originalColor)
 		}
 	}
-
 }
-func distributePixels(image image.Rectangle, n int) []PixelCoordinates {
-	x := image.Max.X
-	y := image.Max.Y
-	result := make([]PixelCoordinates, n)
-	remainders := x * y % n
-	chunkSize := x * y / n
-	firstValue := 0
-	endValue := 0
-	for i := 0; i < n-1; i++ {
-		result[i].MinPixel = findValuePosition(x, y, firstValue)
-		endValue += chunkSize
-		if remainders > 0 {
-			remainders--
-			endValue++
-		}
-		result[i].MaxPixel = findValuePosition(x, y, endValue)
-		firstValue = endValue + 1
+func distributePixels(imageHeight int, n int) [][]int {
+	y := imageHeight
+	if y < n {
+		return [][]int{{0, y}}
 	}
-	result[len(result)-1] = PixelCoordinates{
-		MaxPixel: [2]int{x - 1, y - 1},
-		MinPixel: findValuePosition(x, y, firstValue),
-	}
+	result := make([][]int, n)
 
+	for i := 0; i < n; i++ {
+		startY := (i * y) / n
+		endY := ((i + 1) * y) / n
+		result[i] = []int{startY, endY - 1}
+	}
 	return result
-}
-func findValuePosition(x, y, value int) [2]int {
-	if value > x*y {
-		return [2]int{x - 1, y - 1} // Return the last position if value is out of bounds
-	}
-	if value == 0 {
-		return [2]int{0, 0}
-	}
-	return [2]int{
-		(value - 1) / y,
-		(value - 1) % y,
-	}
-
 }
